3e0a64a0dfb18223c398d877182f51816da7b7a6
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-06-19 17:24:29 -0300

    Get data JPA

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -17,11 +17,11 @@ public class Programa {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
 
-        em.getTransaction().begin();
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
-        em.getTransaction().commit();
+        Pessoa p = em.find(Pessoa.class, 2);
+        System.out.println(p);
+        
         System.out.println("Pronto");
+        em.close();
+        emf.close();
     }
 }
